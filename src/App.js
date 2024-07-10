9fee8ca62b7b861f0def49869f570ebd56b2ea60
Hve <hve4638@gmail.com>
2024-07-11 01:04:52 +0900

    FIX : 시작시 프롬프트를 제대로 불러오지 못하는 문제

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext} from 'react';
+import React, { useEffect, useContext, useState} from 'react';
 import './style.js'
 
 import Home from './pages/Home/Home.tsx'
@@ -11,16 +11,55 @@ import { loadPromptList } from "./services/local/index.ts";
 import { PromptList } from './features/prompts/index.ts';
 
 function App() {
+    const [initialized, setInitialized] = useState(false);
     const promptContext = useContext(PromptContext);
     const stateContext = useContext(StateContext);
     if (promptContext == null) throw new Error('<App/> required PromptContextProvider');
     if (stateContext == null) throw new Error('<App/> required StateContextProvider');
+
+    useEffect(()=>{
+        loadPromptList()
+        .then(data => {
+            const pl = new PromptList(data);
+            promptContext.setPromptList(pl);
+            promptContext.setPrompts(data.prompts);
+            promptContext.setVars(data.vars);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+    }, []);
+  
+    return <div id="app" className='fill theme-dark'>
+        {
+            !initialized &&
+            <Initializer
+                onLoad={()=>setInitialized(true)}
+            />
+        }
+        {
+            initialized &&
+            <Home/>
+        }
+    </div>
+}
+
+function Initializer({ onLoad }) {
+    const [promptsLoaded, setPromptsLoaded] = useState(false);
+    const [promptKeyLoaded, setPromptKeyLoaded] = useState(false);
+    const promptContext = useContext(PromptContext);
+    const stateContext = useContext(StateContext);
+    if (promptContext == null) throw new Error('<PromptContext/> required PromptContextProvider');
+    if (stateContext == null) throw new Error('<StateContext/> required StateContextProvider');
 
     const {
         setPrompt,
         prompt1Key, prompt2Key,
         setPrompt1Key, setPrompt2Key
     } = stateContext;
+    const {
+        promptList
+    } = promptContext;
     
     useEffect(()=>{
         loadPromptList()
@@ -30,20 +69,40 @@ function App() {
             promptContext.setPrompts(data.prompts);
             promptContext.setVars(data.vars);
 
-            const [key1, key2] = pl.findValidPromptKey(prompt1Key, prompt2Key);
-            const item = pl.getPrompt(key1, key2);
-            setPrompt1Key(key1);
-            setPrompt2Key(key2);
-            setPrompt(item);
+            setPromptsLoaded(true);
+        })
+        .catch(err => {
+            console.error(err);
+            setPromptsLoaded(true);
+        })
+    }, []);
+
+
+    useEffect(()=>{
+        loadPromptList()
+        .then(data => {
+            const pl = new PromptList(data);
+            promptContext.setPromptList(pl);
+            promptContext.setPrompts(data.prompts);
+            promptContext.setVars(data.vars);
         })
         .catch(err => {
             console.error(err);
         })
     }, []);
-  
-    return <div id="app" className='fill theme-dark'>
-        <Home></Home>
-    </div>
+
+    useEffect(()=>{
+        console.log('prompt1Key')
+        console.log(prompt1Key)
+        if (promptsLoaded && prompt1Key !== null) {
+            const [key1, key2] = promptList.findValidPromptKey(prompt1Key, prompt2Key);
+            const item = promptList.getPrompt(key1, key2);
+            setPrompt1Key(key1);
+            setPrompt2Key(key2);
+            setPrompt(item);
+            onLoad();
+        }
+    }, [promptsLoaded, prompt1Key])
 }
 
 export default App;
